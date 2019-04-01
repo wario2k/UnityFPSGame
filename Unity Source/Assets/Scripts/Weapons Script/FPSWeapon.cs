35b019fe53dbcde190495c5cd4742ab0d3aa2831
Aayush <shresthaaayush12@gmail.com>
2019-04-01 13:49:50 -0400

    updated guns

---
+++ Unity Source/Assets/Scripts/Weapons Script/FPSWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSWeapon : MonoBehaviour
+{
+
+    private GameObject muzzleFlash;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //tries to find it in the scene and attach it 
+        muzzleFlash = transform.Find("Muzzle Flash").gameObject;
+    }
+
+    // Called when the gun is fired to activate muzzle flash 
+    public void Shoot()
+    {
+        //calls the enumerator to activate the flash and then turn it off in .01 seconds
+        StartCoroutine(TurnOnMuzzleFlash());
+    }
+
+    IEnumerator TurnOnMuzzleFlash()
+    {
+        muzzleFlash.SetActive(true);
+        yield return new WaitForSeconds(0.01f);
+        muzzleFlash.SetActive(false);
+    }
+}
---
+++ Unity Source/Assets/Scripts/Weapons Script/WeaponManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    //will simply hold all the weapons currently in the game
+    public GameObject[] weapons;
+   
+}
