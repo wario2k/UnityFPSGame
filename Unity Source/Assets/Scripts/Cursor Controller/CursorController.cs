d1bc5ebd6491ad1759e6555dcfc2e7390f685c24
Aayush <shresthaaayush12@gmail.com>
2019-04-03 20:55:19 -0400

    added crosshair and improved shooting interactions.

---
+++ Unity Source/Assets/Scripts/Cursor Controller/CursorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //set the cursor to lock onto the crosshairs 
+  
+    void Start()
+    {
+       Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControlCursor();
+    }
+
+    /// <summary>
+    /// Controls the cursor with mouse interactions
+    /// This method locks the cursor with the crosshairs in game.
+    /// </summary>
+    void ControlCursor()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {//if cursor is locked -> unlock it if tab is pressed 
+            if(Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else //lock the cursor 
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+    }
+}
---
+++ Unity Source/Assets/Scripts/FPS Character Script/FPSShootingControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSShootingControls : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private float fireRate = 15f;
+    private float nextTimeToFire = 0f;
+
+    [SerializeField]
+    private GameObject concrete_Impact;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        if(Input.GetMouseButtonDown(0) && Time.time > nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate; //to control rate of fire to avoid firing too fast 
+            //if object is hit -> determined by tracing a ray from the main camera to the direction the camera is pointing at 
+            //will be used to show impact
+            if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out RaycastHit hit)) 
+            {
+                //information about the object that is hit is stored in @RaycastHit hit
+                //using that information we are instantiating a concrete impact game object which shows where we registered the hit
+                Instantiate(concrete_Impact, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+    }
+}
